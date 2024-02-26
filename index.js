2b103433904d33c7aaa9aa7b8171d722135d92b5
MikeJollie2707 <bachngo@yahoo.com>
2024-02-25 18:12:53 -0800

    Allow kb input+move cursor

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// Keys that works as-is: You press, it shows up on the display
+const ALLOWED_REGULAR_KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+// Keys that will create a mathematical function. This includes basic arithmetic operators.
+const ALLOWED_FUNC_KEYS = ['+', '-', '*', '/']
+// Keys that will "do something" to the calculator itself.
+const ALLOWED_OP_KEYS = ["=", "Enter", "Backspace", "ArrowLeft", "ArrowRight"]
+
 function getMainDisplay() {
     return document.getElementById("main-display");
 }
 
 function insertAfterCursor(content) {
     const main_display = getMainDisplay();
+    // Make sure the cursor is visible.
+    main_display.focus();
 
     const cursor_startpos = main_display.selectionStart;
     const cursor_endpos = main_display.selectionEnd;
@@ -13,6 +22,8 @@ function insertAfterCursor(content) {
     }
 
     main_display.value = main_display.value.substring(0, cursor_startpos) + content + main_display.value.substring(cursor_startpos);
+    
+    main_display.selectionStart = main_display.selectionEnd = cursor_startpos + content.length;
 }
 
 function clearDisplay() {
@@ -21,11 +32,25 @@ function clearDisplay() {
 }
 
 function onClick(e) {
-    insertAfterCursor(e.target.innerText);
+    const btn = e.target;
+    handleValue(e.target.innerText);
+    
+    // Without this, pressing Enter after clicking a button will repeat that number.
+    btn.blur();
+}
+function handleValue(value) {
+    insertAfterCursor(value);    
 }
 
 function onFunctionClick(e) {
-    insertAfterCursor(e.target.innerText);
+    const btn = e.target;
+    handleFunction(e.target.innerText);
+    
+    // Without this, pressing Enter after clicking a button will repeat that number.
+    btn.blur();
+}
+function handleFunction(f) {
+    insertAfterCursor(f);
 }
 
 function onOperatorClick(e) {
@@ -41,4 +66,35 @@ function onEvalClick() {
 
     const result = eval(expression);
     console.log(result);
+    clearDisplay();
+    insertAfterCursor(result);
 }
+
+// Allow user keyboard input.
+document.addEventListener("keydown", (e) => {
+    const key = e.key;
+    console.log("Key pressed: %s", key);
+    
+    // Since input field is writable, need to prevent
+    // any input to be inserted to the field.
+    // This is easy with preventDefault().
+    // However, we still want to move the cursor with arrows,
+    // backspace to delete, and more movement related if any.
+    if (ALLOWED_REGULAR_KEYS.includes(key)) {
+        e.preventDefault();
+        handleValue(key);
+    }
+    else if (ALLOWED_FUNC_KEYS.includes(key)) {
+        e.preventDefault();
+        handleFunction(key);
+    }
+    else if (ALLOWED_OP_KEYS.includes(key)) {
+        if (key === "Enter" || key === "=") {
+            e.preventDefault();
+            onEvalClick();
+        }
+    }
+    else {
+        e.preventDefault();
+    }
+})
