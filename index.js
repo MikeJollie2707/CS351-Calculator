c54348871eaefd10472320c1e5f611323f0bcc0c
MikeJollie2707 <bachngo@yahoo.com>
2024-02-25 17:37:40 -0800

    Move text insertion to another function

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ function getMainDisplay() {
     return document.getElementById("main-display");
 }
 
+function insertAfterCursor(content) {
+    const main_display = getMainDisplay();
+
+    const cursor_startpos = main_display.selectionStart;
+    const cursor_endpos = main_display.selectionEnd;
+    if (cursor_startpos !== cursor_endpos) {
+        // TODO: Do something about this.
+        return;
+    }
+
+    main_display.value = main_display.value.substring(0, cursor_startpos) + content + main_display.value.substring(cursor_startpos);
+}
+
 function onClick(e) {
     const main_display = getMainDisplay();
     main_display.value += e.target.innerHTML;
