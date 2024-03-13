9885995d3a6606b2ef73ed5be490dacab8a3f63c
MikeJollie2707 <bachngo@yahoo.com>
2024-03-13 13:51:40 -0700

    Rewrite expression structure

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,18 +1,10 @@
 /**
- * NOTE:
- * Before implementing functions, there are some things to consider:
- * 1. How cursor movement will be decided.
- * - For now, the cursor treat each character as a separate atom. So something like "**" is considered as 2 multiplications by the cursor.
- * - In that sense, for the sake of correctness, the cursor should NOT be able to be placed in between the exponent.
- * - Same reasoning for function. It should NOT be possible to place cursor anywhere in between Math.sin( for example. Either at the start or the end.
- * => A potential solution is to separate the display expr from the inner expr. Let's call this the "logical" expr.
- * For example, the expression 5-(3+6) will be stored in logical expr as ['5', '-', '(', '3', '+', '6', ')']
- * Another example, the expr 5+Math.sin(0.5) is stored as ['5', '+', 'Math.sin(', '0', '.', '5', ')']
- * The "logical" cursor is therefore an index pointing to this logical expr. Wherever it is pointing, the display cursor will be right after it.
- * For example, the expr 5-|(3+6) with vertical bar as cursor will correspond to logical cursor being 1, pointing to the '-'.
- * Ofc we'll need to translate the logical cursor and expr into display cursor and expr. There will be functions for that.
- * This will ensure that when we backspace a function, we'll delete the entire function in one go.
- * 
+ * 2 displays: main display (where user input happens) and result display (only show result)
+ * result display is very simple, it has almost no abstraction.
+ * main display has an abstraction layer.
+ */
+
+/**
  * 2. Should exponent be ** or Math.pow()
  * - ** is only recommended after implementing point 1.
  * 
@@ -21,6 +13,9 @@
  * 
  */
 
+const logical_expr = [];
+let cursor_ptr = -1;
+
 export function getMainDisplay() {
     return document.getElementById("main-display");
 }
@@ -28,73 +23,69 @@ export function getResultDisplay() {
     return document.getElementById("result-display");
 }
 
-/**
- * Insert content in main display after wherever the cursor is.
- * 
- * @param {string} content Content to insert in main display.
- * @param {number | null} cursor_offset How much distance from where the cursor was to move. Default to the length of `content`.
- */
-export function insertAfterCursor(content, cursor_offset=null) {
-    const main_display = getMainDisplay();
-    // Make sure the cursor is visible.
-    main_display.focus();
-    doIfResultPresent([clearMainDisplay, clearResultDisplay]);
-    
-    const cursor_startpos = main_display.selectionStart;
-    const cursor_endpos = main_display.selectionEnd;
-    // if start == end, then we simply insert in between left of cursor and right of cursor, which is [0, i) and [i, ...]
-    // if start != end, selectionEnd is pass the last selected character, so we can also use [0, i) and [i, ...] to ignore selected characters.
-    main_display.value = main_display.value.substring(0, cursor_startpos) + content + main_display.value.substring(cursor_endpos);
-    
-    // Move the cursor by a distance from its current position.
-    // By default we want to move it to the length of the content
-    // so it'll appear after the newly inserted content.
-    const cursor_dist = cursor_offset !== null ? cursor_offset : content.length;
-    const new_cursor_pos = cursor_startpos + cursor_dist;
-
-    // Don't remove this.
-    // Fix #1
-    main_display.blur();
-    main_display.focus();
-
-    main_display.setSelectionRange(new_cursor_pos, new_cursor_pos);
-}
-
-export function deleteBeforeCursor(range=1) {
-    const main_display = getMainDisplay();
-    main_display.focus();
-    doIfResultPresent([clearMainDisplay, clearResultDisplay]);
-
-    const cursor_startpos = main_display.selectionStart;
-    const cursor_endpos = main_display.selectionEnd;
-
-    if (cursor_endpos !== cursor_startpos) {
-        // TODO: Handle this case.
-        return;
-    }
-
-    main_display.value = main_display.value.substring(0, cursor_startpos - range) + main_display.value.substring(cursor_endpos);
-    
-    let new_cursor_pos = cursor_startpos - range;
-    if (new_cursor_pos < 0) {
-        new_cursor_pos = 0;
-    }
-    main_display.setSelectionRange(new_cursor_pos, new_cursor_pos);
-}
-
 export function writeResult(result) {
     const res_display = getResultDisplay();
     res_display.value = result;
 }
 
-export function clearMainDisplay() {
-    const main_display = getMainDisplay();
-    main_display.value = "";
-}
-
 export function clearResultDisplay() {
     const res_display = getResultDisplay();
     res_display.value = "";
+}
+
+export function render() {
+    const main_display = getMainDisplay();
+
+    const expr = logical_expr.join('');
+
+    let cursor_pos = 0;
+    for (let i = 0; i <= cursor_ptr; ++i) {
+        cursor_pos += logical_expr[i].length;
+    }
+    
+    main_display.value = expr;
+    
+    main_display.blur();
+    main_display.focus();
+    main_display.setSelectionRange(cursor_pos, cursor_pos);
+}
+
+export function moveCursor(offset) {
+    // TODO: Reorganize this code.
+    if (cursor_ptr + offset >= logical_expr.length - 1) {
+        cursor_ptr = logical_expr.length - 1;
+        return;
+    }
+    if (cursor_ptr + offset < -1) {
+        cursor_ptr = -1;
+        return;
+    }
+    cursor_ptr += offset;
+}
+
+export function insert(content) {
+    doIfResultPresent([clearLogicalExpr, clearResultDisplay]);
+    logical_expr.splice(cursor_ptr + 1, 0, content);
+    moveCursor(1);
+
+    console.log(logical_expr);
+}
+export function deleteBeforeCaret() {
+    doIfResultPresent([clearLogicalExpr, clearResultDisplay]);
+    if (cursor_ptr < 0) {
+        return;
+    }
+    logical_expr.splice(cursor_ptr, 1);
+    moveCursor(-1);
+
+    console.log(logical_expr);
+}
+export function clearLogicalExpr() {
+    logical_expr.splice(0, logical_expr.length);
+    cursor_ptr = -1;
+}
+export function getLogicalExpr() {
+    return logical_expr;
 }
 
 /**
