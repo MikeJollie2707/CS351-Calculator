2b0eaaaeae6060b258de0a0eb0168795db70faa9
MikeJollie2707 <bachngo@yahoo.com>
2024-03-13 13:52:18 -0700

    Move operators into callbacks

---
+++ scripts/operators.js
@@ -0,0 +1,72 @@
+import * as display from "./controllers.js";
+import * as hist from "./history.js";
+
+export function evaluate() {
+    const main_display = display.getMainDisplay();
+    const expression = main_display.value;
+
+    try {
+        const result = eval(expression);
+        console.log(result);
+        display.writeResult(result);
+        hist.addHistory({expression: Array.from(display.getLogicalExpr()), result: result});
+        
+        main_display.blur();
+    }
+    catch {
+        display.writeResult("Error");
+    }
+}
+
+export function clearScreen() {
+    display.clearLogicalExpr();
+    display.clearResultDisplay();
+    display.render();
+
+    hist.toPresent();
+}
+
+export function del() {
+    display.deleteBeforeCaret();
+    display.render();
+
+    hist.toPresent();
+}
+
+export function showLastHistory() {
+    const history = hist.getPreviousHistory();
+    if (!history) {
+        return;
+    }
+
+    displayHistory(history);    
+}
+
+export function showNextHistory() {
+    const history = hist.getNextHistory();
+    if (!history) {
+        return;
+    }
+
+    displayHistory(history);
+}
+
+export function moveCursorLeft() {
+    display.moveCursor(-1);
+    display.render();
+}
+
+export function moveCursorRight() {
+    display.moveCursor(1);
+    display.render();
+}
+
+function displayHistory(history) {
+    const {expression, result} = history;
+    display.clearLogicalExpr();
+    for (const atom of expression) {
+        display.insert(atom);
+    }
+    display.render();
+    display.writeResult(result);
+}
